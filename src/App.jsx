35636f434777c15b1e4540f61a26c6580823ef36
unknown <rajujeelaga@gmail.com>
2024-12-16 16:35:18 +0530

    Tab Buttons functionality by using props and childrens and using State
    Tab Buttons functionality by using props and children and State option as well, and taking the data.jsx sample data and get that data in APP file using above concepts

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,18 @@
+ import { useState } from 'react'; 
 import Header from '../src/components/Header/Header.jsx';
 import { CORE_CONCEPTS } from '../src/data.jsx';
 import CoreConcepts from '../src/components/CoreConcepts.jsx';
+import TabButtons from './components/TabButtons.jsx';
+import { EXAMPLES } from '../src/data.jsx';
 
 function App() {
+  const [selectedTopic, setSelectedTopic] = useState('components');
+
+  function handleClick(selectedButton){
+    setSelectedTopic(selectedButton);
+    console.log(selectedButton);
+  }
+
   return (
     <div>
       <Header></Header>
@@ -20,10 +30,40 @@ function App() {
             <CoreConcepts {...CORE_CONCEPTS[3]}/>
             </ul>
         </section>
-        
+        <section id="examples">
+          <h2>Examples</h2>
+          <menu>
+            <TabButtons onSelect={() => handleClick('components')}>Components</TabButtons>
+            <TabButtons onSelect={() => handleClick('jsx')}>JSX</TabButtons>
+            <TabButtons onSelect={() => handleClick('props')}>Props</TabButtons>
+            <TabButtons onSelect={() => handleClick('state')}>State</TabButtons>
+          </menu>
+        </section>
+        <div id="tab-content">
+          <h3>{EXAMPLES[selectedTopic].title}</h3>
+          <p>{EXAMPLES[selectedTopic].description}</p>
+          <pre>
+            <code>{EXAMPLES[selectedTopic].code}</code>
+          </pre>
+        </div>
       </main>
     </div>
   );
 }
 
 export default App;
+
+// Task 1
+// export default function App() {
+//   const [selectPrice, setselectPrice] = useState('$100');
+  
+//   function handleClick(DiscountPrice){
+//       setselectPrice(DiscountPrice);
+//   }
+//  return (
+//      <div>
+//          <p data-testid="price">{selectPrice}</p>
+//          <button onClick={() => handleClick('$75')}>Apply Discount</button>
+//      </div>
+//  );
+// }
---
+++ src/components/TabButtons.jsx
@@ -0,0 +1,7 @@
+export default function TabButtons({children, onSelect}){
+    return (
+        <li>
+            <button onClick={onSelect}>{children}</button>
+        </li>
+    )
+}
