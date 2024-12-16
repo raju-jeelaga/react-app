08c73ac63a3498c3ba72cdcf69b0b4701cf6fe35
unknown <rajujeelaga@gmail.com>
2024-12-16 10:33:15 +0530

    props concept basic model
    props concept basic model

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,33 @@
 import Header from '../src/components/Header/Header.jsx';
+import CORE_CONCEPTS_IMAGE from '../src/assets/react-core-concepts.png';
 
+function CoreConcepts(props){
+  return (
+      <li>
+        <img src={props.image} alt="Stylized atom" />
+        <h3>{props.title}</h3>
+        <p>{props.description}</p>
+      </li>
+  );
+}
 function App() {
   return (
     <div>
       <Header></Header>
       <main>
-        <h2>Time to get started!</h2>
+        <section id="core-concepts">
+          <h2>Core Concepts</h2>
+          <ul>
+          <CoreConcepts 
+            title="Components" 
+            description="The core UI building block - compose the user interface by combining multiple components."
+            image={CORE_CONCEPTS_IMAGE}></CoreConcepts>
+            <CoreConcepts></CoreConcepts>
+            <CoreConcepts></CoreConcepts>
+            <CoreConcepts></CoreConcepts>
+            </ul>
+        </section>
+        
       </main>
     </div>
   );
