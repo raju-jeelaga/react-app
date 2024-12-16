277f54b48c389a1ab01bb49ee80a282800eb26b8
unknown <rajujeelaga@gmail.com>
2024-12-16 10:44:31 +0530

    Using JSON file directly calling the data
    Using JSON file directly getting the data using props concept

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import Header from '../src/components/Header/Header.jsx';
-import CORE_CONCEPTS_IMAGE from '../src/assets/react-core-concepts.png';
+//import CORE_CONCEPTS_IMAGE from '../src/assets/react-core-concepts.png';
+import { CORE_CONCEPTS } from '../src/data.jsx';
 
 function CoreConcepts(props){
   return (
       <li>
-        <img src={props.image} alt="Stylized atom" />
+        <img src={props.image} alt={props.title} />
         <h3>{props.title}</h3>
         <p>{props.description}</p>
       </li>
@@ -18,13 +19,26 @@ function App() {
         <section id="core-concepts">
           <h2>Core Concepts</h2>
           <ul>
-          <CoreConcepts 
-            title="Components" 
-            description="The core UI building block - compose the user interface by combining multiple components."
-            image={CORE_CONCEPTS_IMAGE}></CoreConcepts>
-            <CoreConcepts></CoreConcepts>
-            <CoreConcepts></CoreConcepts>
-            <CoreConcepts></CoreConcepts>
+            <CoreConcepts 
+             title={CORE_CONCEPTS[0].title} 
+             description={CORE_CONCEPTS[0].description}
+             image={CORE_CONCEPTS[0].image}
+            />
+            <CoreConcepts 
+             title={CORE_CONCEPTS[1].title} 
+             description={CORE_CONCEPTS[1].description}
+             image={CORE_CONCEPTS[1].image}
+            />
+            <CoreConcepts 
+             title={CORE_CONCEPTS[2].title} 
+             description={CORE_CONCEPTS[2].description}
+             image={CORE_CONCEPTS[2].image}
+            />
+            <CoreConcepts 
+             title={CORE_CONCEPTS[3].title} 
+             description={CORE_CONCEPTS[3].description}
+             image={CORE_CONCEPTS[3].image}
+            />
             </ul>
         </section>
         
