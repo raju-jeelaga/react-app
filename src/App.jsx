86c4a2268e9c11a9e775cf724478053e61749dfa
unknown <rajujeelaga@gmail.com>
2024-12-16 10:52:07 +0530

    To get the data,jsx JSON Data another way
    To get the data,jsx JSON Data another approach using spread operator and instead of props for short maner I use Object Destructuring concepts

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,12 @@ import Header from '../src/components/Header/Header.jsx';
 //import CORE_CONCEPTS_IMAGE from '../src/assets/react-core-concepts.png';
 import { CORE_CONCEPTS } from '../src/data.jsx';
 
-function CoreConcepts(props){
+function CoreConcepts({image, title, description}){
   return (
       <li>
-        <img src={props.image} alt={props.title} />
-        <h3>{props.title}</h3>
-        <p>{props.description}</p>
+        <img src={image} alt={title} />
+        <h3>{title}</h3>
+        <p>{description}</p>
       </li>
   );
 }
@@ -24,21 +24,9 @@ function App() {
              description={CORE_CONCEPTS[0].description}
              image={CORE_CONCEPTS[0].image}
             />
-            <CoreConcepts 
-             title={CORE_CONCEPTS[1].title} 
-             description={CORE_CONCEPTS[1].description}
-             image={CORE_CONCEPTS[1].image}
-            />
-            <CoreConcepts 
-             title={CORE_CONCEPTS[2].title} 
-             description={CORE_CONCEPTS[2].description}
-             image={CORE_CONCEPTS[2].image}
-            />
-            <CoreConcepts 
-             title={CORE_CONCEPTS[3].title} 
-             description={CORE_CONCEPTS[3].description}
-             image={CORE_CONCEPTS[3].image}
-            />
+            <CoreConcepts {...CORE_CONCEPTS[1]}/>
+            <CoreConcepts {...CORE_CONCEPTS[2]}/>
+            <CoreConcepts {...CORE_CONCEPTS[3]}/>
             </ul>
         </section>
         
