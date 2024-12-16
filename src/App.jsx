3e8c0871e893d026c21c805be7837582fb2e1ac8
unknown <rajujeelaga@gmail.com>
2024-12-16 17:31:15 +0530

    Outputting List Data Dynamically
    Outputting List Data Dynamically, if any content is missing, the output data will adjust automatically

--- src/App.jsx
+++ src/App.jsx
@@ -20,14 +20,17 @@ function App() {
         <section id="core-concepts">
           <h2>Core Concepts</h2>
           <ul>
-            <CoreConcepts 
+            {CORE_CONCEPTS.map( (conceptItem) => (
+              <CoreConcepts key={conceptItem.title} {...conceptItem} />
+            ))}
+            {/* <CoreConcepts 
              title={CORE_CONCEPTS[0].title} 
              description={CORE_CONCEPTS[0].description}
              image={CORE_CONCEPTS[0].image}
             />
             <CoreConcepts {...CORE_CONCEPTS[1]}/>
             <CoreConcepts {...CORE_CONCEPTS[2]}/>
-            <CoreConcepts {...CORE_CONCEPTS[3]}/>
+            <CoreConcepts {...CORE_CONCEPTS[3]}/> */}
             </ul>
         </section>
         <section id="examples">
